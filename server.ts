ef45db6897e2d4df2f9c1ec77d6faab78b131958
chamika-damith <chamikadamith9@gmail.com>
2025-01-24 22:09:01 +0530

    equipment manage crud functions complete

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client';
 import {cropRoute} from "./routes/crop-route";
 import {staffRoute} from "./routes/staff-route";
 import {vehicleRoute} from "./routes/vehicle-route";
+import {equipmentRoute} from "./routes/equipment-route";
 
 const app=express();
 const prisma = new PrismaClient();
@@ -16,6 +17,7 @@ app.use('/field',fieldRoute)
 app.use('/crop',cropRoute)
 app.use('/staff',staffRoute)
 app.use('/vehicle',vehicleRoute)
+app.use('/equipment',equipmentRoute)
 
 async function connectDatabase() {
     try {
