76e65d6064b492b6228f55456e7a300d7141ae40
chamika-damith <chamikadamith9@gmail.com>
2025-01-24 12:25:13 +0530

    field manage crud functions complete

--- server.ts
+++ server.ts
@@ -1,13 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import {fieldRoute} from "./routes/field-route";
+import { PrismaClient } from '@prisma/client';
 
 const app=express();
+const prisma = new PrismaClient();
 
 app.use(express.json())
 app.use(cors());
 
 app.use('/field',fieldRoute)
+
+async function connectDatabase() {
+    try {
+        await prisma.$connect();
+        console.log('Connected to Database');
+    } catch (error) {
+        console.error('Error connecting to database', error);
+    }
+}
+
+connectDatabase();
+
 
 app.listen(3000,(err)=>{
     console.log("Server running on port 3000")
